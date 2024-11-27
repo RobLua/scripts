c6ad05735d24283ff71a44b068ce67a3bfba89c8
RobLua <dtokar2008@gmail.com>
2024-11-27 05:47:40 +0300

    Update yba.lua

--- yba.lua
+++ yba.lua
@@ -1,1 +1,2 @@
-loadstring(game:HttpGet"https://raw.githubusercontent.com/RobLua/scripts/main/gui.lua")()
+local a = loadstring(game:HttpGet"https://raw.githubusercontent.com/RobLua/scripts/main/gui.lua")()
+print(a)
