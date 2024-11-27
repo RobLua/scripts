ac6592073f4ad3dc5d633f4250c4619214875e73
RobLua <dtokar2008@gmail.com>
2024-11-27 05:45:08 +0300

    Update yba.lua

--- yba.lua
+++ yba.lua
@@ -1,1 +1,1 @@
-loadstring("https://raw.githubusercontent.com/dyyll/Dex-V5-leak/refs/heads/main/Dex%20V5.lua")()
+loadstring(game:HttpGet"https://raw.githubusercontent.com/dyyll/Dex-V5-leak/refs/heads/main/Dex%20V5.lua")()
