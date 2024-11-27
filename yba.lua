48c97ab661e2e74aa5912824c0b17f253521ad84
RobLua <dtokar2008@gmail.com>
2024-11-27 06:18:13 +0300

    Update yba.lua

--- yba.lua
+++ yba.lua
@@ -1,5 +1,5 @@
 warn("new")
-print("yo")
+print("yo1")
 local player = game.Players.LocalPlayer
 local module = loadstring(game:HttpGet"https://raw.githubusercontent.com/RobLua/scripts/main/gui.lua")()
 local button = module.CreateButton()
