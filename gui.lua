10ca41aacdee82a18db5fff785c9bc0826a62313
RobLua <dtokar2008@gmail.com>
2024-11-27 04:16:11 +0300

    Update gui.lua

--- gui.lua
+++ gui.lua
@@ -1,12 +1,6 @@
--- /scripts/yba/gui.lua
-local ScreenGui = Instance.new("ScreenGui")
-ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+-- gui.lua
 
-local Button = Instance.new("TextButton")
-Button.Size = UDim2.new(0, 200, 0, 50)
-Button.Position = UDim2.new(0.5, -100, 0.5, -25)
-Button.Text = "Click Me!"
-Button.Parent = ScreenGui
-
--- Сохраняем кнопку в глобальной таблице для доступа из другого скрипта
-_G.ClickButton = Button
+-- Определяем глобальную функцию
+_G.showMessage = function(message)
+    print("[GUI]: " .. message)
+end
