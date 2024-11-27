914bbd1c2e64f4383540b463bbb76e461efe81db
RobLua <dtokar2008@gmail.com>
2024-11-27 06:17:51 +0300

    Update yba.lua

--- yba.lua
+++ yba.lua
@@ -3,6 +3,4 @@ print("yo")
 local player = game.Players.LocalPlayer
 local module = loadstring(game:HttpGet"https://raw.githubusercontent.com/RobLua/scripts/main/gui.lua")()
 local button = module.CreateButton()
-button.MouseButton1Click:Connect(function()
-    print("Кнопка нажата!")
-end)
+
