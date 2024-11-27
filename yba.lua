8f99277ea9258deeee293406ed2a682abb61591b
RobLua <dtokar2008@gmail.com>
2024-11-27 04:15:06 +0300

    Update yba.lua

--- yba.lua
+++ yba.lua
@@ -1,2 +1,14 @@
-print("test")
-loadstring(print("sigma"))
+-- script.lua
+
+-- Подключаем gui.lua с помощью loadfile
+local guiFile = loadfile("gui.lua") -- Указываем путь к gui.lua
+if guiFile then
+    guiFile() -- Выполняем gui.lua
+end
+
+-- Теперь используем переменные или функции, определенные в gui.lua
+if _G.showMessage then
+    _G.showMessage("Привет из script.lua!")
+else
+    print("Функция showMessage не найдена!")
+end
